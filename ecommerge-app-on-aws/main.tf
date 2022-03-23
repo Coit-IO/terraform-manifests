f23dd712b1acb910d57d5e0d547cb6777b354e64
basil <basil1987@gmail.com>
2022-03-23 21:22:12 +0530

    to share with students

--- ecommerge-app-on-aws/main.tf
+++ ecommerge-app-on-aws/main.tf
@@ -20,6 +20,7 @@ resource "aws_subnet" "subnet_1a" {
   vpc_id     = aws_vpc.ecommerce-vpc.id
   cidr_block = "10.10.1.0/24"
   availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "subnet-1a"
@@ -44,52 +45,65 @@ resource "aws_subnet" "subnet_1c" {
   }
 }
 
-resource "aws_instance" "web001" {
-  ami = "ami-000051d5c1a3d7008"
-  instance_type = "t2.micro"
+
+resource "aws_internet_gateway" "myvpc_ig" {
+  vpc_id = aws_vpc.ecommerce-vpc.id
+
   tags = {
-    Name = "web001"
-    App = "Wordpress"
+    Name = "myvpc_ig"
   }
-  subnet_id = aws_subnet.subnet_1a.id
-  vpc_security_group_ids = [aws_security_group.allow_port80.id]
 }
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.ecommerce-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myvpc_ig.id
+  }
+
+  tags = {
+    Name = "rt_public"
+  }
+}
+
+resource "aws_route_table_association" "associate-1a-rt" {
+  subnet_id      = aws_subnet.subnet_1a.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "associate-1b-rt" {
+  subnet_id      = aws_subnet.subnet_1b.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_instance" "web001" {
+  ami           = "ami-0851b76e8b1bce90b"
+  instance_type = "t3.micro"
+  subnet_id = aws_subnet.subnet_1a.id
+
+  tags = {
+    Name = "Web001"
+  }
+}
+
 
 resource "aws_instance" "web002" {
-  ami = "ami-000051d5c1a3d7008"
+  ami           = "ami-0851b76e8b1bce90b"
   instance_type = "t2.micro"
-  tags = {
-    Name = "web002"
-    App = "Wordpress"
-  }
   subnet_id = aws_subnet.subnet_1b.id
-  vpc_security_group_ids = [aws_security_group.allow_port80.id]
+  tags = {
+    Name = "Web002"
+  }
 }
 
-resource "aws_security_group" "allow_port80" {
-  name        = "allow-port80"
-  description = "Allow port 80 inbound traffic"
-  vpc_id      = aws_vpc.ecommerce-vpc.id
-
+resource "aws_security_group" "aws_default_sg" {
+  description = "default VPC security group"
   ingress {
-    description      = "HTTP from Internet"
+    description      = "HTTP from ALL Internet"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
   }
 }
-
