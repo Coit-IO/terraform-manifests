e95a13868d36173b3cb3b556be1c584b09de718a
basil <basil1987@gmail.com>
2022-04-04 20:11:33 +0530

    Final AWS infrastructure

--- ecommerge-app-on-aws/main.tf
+++ ecommerge-app-on-aws/main.tf
@@ -31,6 +31,7 @@ resource "aws_subnet" "subnet_1b" {
   vpc_id     = aws_vpc.ecommerce-vpc.id
   cidr_block = "10.10.2.0/24"
   availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = "true"
   tags = {
     Name = "subnet-1b"
   }
@@ -78,10 +79,11 @@ resource "aws_route_table_association" "associate-1b-rt" {
 }
 
 resource "aws_instance" "web001" {
-  ami           = "ami-0851b76e8b1bce90b"
+  ami           = "ami-0cfe39d5e0c8e331a"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.subnet_1a.id
-
+  vpc_security_group_ids = [ aws_security_group.webservers.id ]
+  key_name = "mar22"
   tags = {
     Name = "Web001"
   }
@@ -89,11 +91,24 @@ resource "aws_instance" "web001" {
 
 
 resource "aws_instance" "web002" {
-  ami           = "ami-0851b76e8b1bce90b"
+  ami           = "ami-0cfe39d5e0c8e331a"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_1b.id
+  vpc_security_group_ids = [ aws_security_group.webservers.id ]
+  key_name = "mar22"
   tags = {
     Name = "Web002"
+  }
+}
+
+resource "aws_instance" "web003" {
+  ami           = "ami-0cfe39d5e0c8e331a"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet_1b.id
+  vpc_security_group_ids = [ aws_security_group.webservers.id ]
+  key_name = "mar22"
+  tags = {
+    Name = "Web003"
   }
 }
 
@@ -106,4 +121,162 @@ resource "aws_security_group" "aws_default_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "webservers" {
+  name        = "webservers-80"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.ecommerce-vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_lb_target_group" "mywebservergroup" {
+  name     = "webservergroup"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.ecommerce-vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "attach_web001_tg" {
+  target_group_arn = aws_lb_target_group.mywebservergroup.arn
+  target_id        = aws_instance.web001.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach_web002_tg" {
+  target_group_arn = aws_lb_target_group.mywebservergroup.arn
+  target_id        = aws_instance.web002.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach_web003_tg" {
+  target_group_arn = aws_lb_target_group.mywebservergroup.arn
+  target_id        = aws_instance.web003.id
+  port             = 80
+}
+
+resource "aws_lb" "lb-webservers" {
+  name               = "lb-webservers"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_webservers.id]
+  subnets            = [ aws_subnet.subnet_1a.id, aws_subnet.subnet_1b.id ]
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "front_end_80" {
+  load_balancer_arn = aws_lb.lb-webservers.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mywebservergroup.arn
+  }
+}
+
+
+
+resource "aws_security_group" "lb_webservers" {
+  name        = "lb-webservers-80"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.ecommerce-vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_launch_configuration" "ecommerce-lc" {
+  name          = "ecommerce-lc"
+  image_id      = "ami-0cfe39d5e0c8e331a"
+  instance_type = "t2.micro"
+  security_groups = [ aws_security_group.webservers.id ]
+  key_name = "mar22"
+}
+
+variable "asgMin" {
+  type = string
+  default = "1"
+}
+
+variable "asgMax" {
+  type = string
+  default = "5"
+}
+
+variable "asgDesired" {
+  type = string
+  default = "2"
+}
+
+resource "aws_autoscaling_group" "ecommerce-asg" {
+  name                      = "ecommerce-asg"
+  max_size                  = "${var.asgMax}"
+  min_size                  = "${var.asgMin}"
+  desired_capacity          = "${var.asgDesired}"
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.ecommerce-lc.name
+  vpc_zone_identifier       = [aws_subnet.subnet_1a.id, aws_subnet.subnet_1b.id]
+
+  tag {
+    key                 = "foo"
+    value               = "bar"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+  tag {
+    key                 = "lorem"
+    value               = "ipsum"
+    propagate_at_launch = false
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.ecommerce-asg.id
+  alb_target_group_arn   = aws_lb_target_group.mywebservergroup.arn
 }
